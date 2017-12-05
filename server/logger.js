e692685ee0cfe86f06a6a66c4d293fdda49fadc2
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2017-12-05 21:22:38 -0200

    Change logger format
    Change logger format to be:
    
    Files: <timestamp> [<log level>]: <message>
    Console: <message>

--- server/logger.js
+++ server/logger.js
@@ -1,17 +1,22 @@
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
 
-const logger = winston.createLogger({
+const { combine, timestamp, printf } = format;
+
+const logger = createLogger({
 	level: 'info',
-	format: winston.format.json(),
+	format: combine(
+		timestamp(),
+		printf(info => `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`)
+	),
 	transports: [
-		new winston.transports.File({ filename: 'error.log', level: 'error' }),
-		new winston.transports.File({ filename: 'console.log' })
+		new transports.File({ filename: 'error.log', level: 'error' }),
+		new transports.File({ filename: 'console.log' })
 	]
 });
 
 if (process.env.NODE_ENV !== 'production') {
-	logger.add(new winston.transports.Console({
-		format: winston.format.simple()
+	logger.add(new transports.Console({
+		format: printf(info => info.message)
 	}));
 }
 
