845f7d052e970334cc2b170b815f11ddbd9d2ccb
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2017-12-06 22:05:18 -0200

    Fix 'logger.info is unary'
    Fix the issue using format.splat() function which allows
    string arguments to be passed to the logger, as referenced at
    https://github.com/winstonjs/winston#string-interpolation

--- server/logger.js
+++ server/logger.js
@@ -1,12 +1,15 @@
 const { createLogger, format, transports } = require('winston');
 
-const { combine, timestamp, printf } = format;
+const {
+	combine, timestamp, printf, splat
+} = format;
 
 const logger = createLogger({
 	level: 'info',
 	format: combine(
 		timestamp(),
-		printf(info => `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`)
+		splat(),
+		printf(log => `${log.timestamp} [${log.level.toUpperCase()}]: ${log.message}`)
 	),
 	transports: [
 		new transports.File({ filename: 'error.log', level: 'error' }),
@@ -16,7 +19,10 @@ const logger = createLogger({
 
 if (process.env.NODE_ENV !== 'production') {
 	logger.add(new transports.Console({
-		format: printf(info => info.message)
+		format: combine(
+			splat(),
+			printf(log => log.message)
+		)
 	}));
 }
 
