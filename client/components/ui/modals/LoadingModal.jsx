edc9d033563e65b9aa4e9499b565232984da59af
Henrique N Facioli <henrique.facioli@gmail.com>
2018-01-12 15:34:27 -0200

    Loading Implementation: Spinner, Modal, Page

---
+++ client/components/ui/modals/LoadingModal.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// import Icon from './Icon';
+import Modal from '../Modal';
+import Spinner from '../Spinner';
+import strings from '../../../../shared/strings';
+
+import '../../../styles/pageLoading.styl';
+
+const LoadingModal = props => (
+	<Modal
+		active={props.active}
+		title={props.title ? props.title : strings.pageLoading}
+		content={<Spinner class="loadingModal" />}
+		hasButtons={false}
+	/>
+);
+
+LoadingModal.propTypes = {
+	active: PropTypes.bool,
+	title: PropTypes.string
+};
+
+LoadingModal.defaultProps = {
+	active: false,
+	title: null
+};
+
+export default LoadingModal;
