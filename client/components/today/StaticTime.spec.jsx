48581034a13732f95b9cbff54201fdd307fce67f
henriquefacioli <henrique.facioli@gmail.com>
2018-02-02 15:45:19 -0200

    StaticTime test

---
+++ client/components/today/StaticTime.spec.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import StaticTime from './StaticTime';
+
+describe('StaticTime', () => {
+	describe('render', () => {
+		it('should render properly with minimum props', () => {
+			const wrapper = shallow(<StaticTime label="entry" emphasis />);
+			expect(wrapper).toMatchSnapshot();
+		});
+	})
+	describe('display time', () => {
+		it('should display time if anything comes', () => {
+			const propTime = { hours: "", minutes: 10};
+			const wrapper = shallow(<StaticTime
+				time={propTime}
+				label="entry"
+				emphasis
+			/>);
+			expect(wrapper).toMatchSnapshot();
+		})
+	});
+});
+
