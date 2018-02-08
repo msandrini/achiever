94e5d4b03d690f4283de8e90e9763981296d3ac6
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2018-02-08 19:44:13 +0000

    Fix middleware userDetails api
    It was returnin the wrong balance. So this fixes it, and add unit test to cover it.

---
+++ server/api/middleware.spec.js
@@ -0,0 +1,80 @@
+const cheerio = require('cheerio');
+const {
+	getBalance
+} = require('./middleware');
+
+const generateTableLine = (date, balance) => (`
+<tr>
+	<td>${date}</td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td>&nbsp;${balance}</td>
+	<td></td>
+</tr>
+`);
+
+const TABLE_HEADER = `
+	<tr>
+		<th>Date</th>
+		<th>Contracted</th>
+		<th>Entry</th>
+		<th>Working Time</th>
+		<th>Breaks</th>
+		<th>End time</th>
+		<th>Daily Compensation</th>
+		<th>Daily Overtime</th>
+		<th>Balance</th>
+		<th>Remarks</th>
+	</tr>
+`
+
+describe('middleware', () => {
+	describe('getBalance()', () => {
+		const targetBalance = '2:00';
+
+		it('should return last FRIDAY 8th td WHEN first tr-td is MONDAY', () => {
+			const baseHtml = `
+				<table>
+					${ generateTableLine('2018-02-05 Mon', '5:00') }
+					${ generateTableLine('2018-02-02 Fri', targetBalance) }
+				</table>
+			`;			
+			const $ = cheerio.load(baseHtml);
+			const result = getBalance($);
+			expect(result).toEqual(targetBalance);
+		});
+
+		it('should return last FRIDAY 8th td WHEN it has header in the middle', () => {
+			const baseHtml = `
+				<table>
+					${ generateTableLine('2018-02-07 Web', '7:00') }
+					${ TABLE_HEADER }
+					${ generateTableLine('2018-02-06 Tue', '6:00') }
+					${ TABLE_HEADER }
+					${ generateTableLine('2018-02-05 Mon', '5:00') }
+					${ TABLE_HEADER }
+					${ generateTableLine('2018-02-02 Fri', targetBalance) }
+				</table>
+			`;			
+			const $ = cheerio.load(baseHtml);
+			const result = getBalance($);
+			expect(result).toEqual(targetBalance);
+		});
+
+		it('should return 8th td when the first tr-td is FRIDAY', () => {
+			const baseHtml = `
+				<table>
+					${ generateTableLine('2018-02-02 Fri', targetBalance) }
+				</table>
+			`;			
+			const $ = cheerio.load(baseHtml);
+			const result = getBalance($);
+			expect(result).toEqual(targetBalance);
+		});
+	});
+});
