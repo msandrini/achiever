e19afd36346c34b08fe2fbbace41092bce41c17f
henriquefacioli <henrique.facioli@gmail.com>
2018-02-14 11:05:51 -0200

    ActiveDayTime tests

---
+++ client/components/edit/ActiveDayTimes.spec.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ActiveDayTimes from './ActiveDayTimes';
+
+describe('ActiveDayTimes', () => {
+	describe('render', () => {
+		it('should render 4 TimeGroups', () => {
+			const wrapper = shallow(<ActiveDayTimes onTimeChange={() => {}} />);
+			expect(wrapper).toMatchSnapshot();
+
+			const wrapperFocus = shallow(<ActiveDayTimes onTimeChange={() => {}} />);
+			wrapperFocus.setState({ shouldHaveFocus: { fieldMode: 'hours', index: 1 } });
+			expect(wrapper).toMatchSnapshot();
+
+			const { onFocus } = wrapperFocus.find('TimeGroup').at(1).props();
+			onFocus('hours');
+			expect(wrapperFocus.state('focusedField')).toEqual({ index: 1, fieldMode: 'hours' });
+		});
+	});
+	describe('togglePause', () => {
+		it('should hide lunch fields on check', () => {
+			const wrapper = shallow(<ActiveDayTimes onTimeChange={() => () => {}} />);
+			const { onCheck } = wrapper.find('CheckBox').props();
+			onCheck(true);
+			expect(wrapper).toMatchSnapshot();
+		});
+	});
+	describe('onChangeTime', () => {
+		it('should call onTimeChange with the with correct params', () => {
+			const onTimeChangeIntern = jest.fn();
+			const onTimeChange = jest.fn(() => onTimeChangeIntern);
+			const wrapper = shallow(<ActiveDayTimes onTimeChange={onTimeChange} />);
+			const { onSet } = wrapper.find('TimeGroup').at(2).props();
+			onSet(10, 10);
+			expect(onTimeChangeIntern).toHaveBeenCalledWith(10, 10);
+			expect(onTimeChange).toBeCalledWith(2);
+		});
+
+		describe('AutoTab', () => {
+			it('should get the nextField', () => {
+				const onTimeChangeIntern = jest.fn();
+				const onTimeChange = jest.fn(() => onTimeChangeIntern);
+				const focusOnSubmit = jest.fn();
+
+				const wrapper = shallow(<ActiveDayTimes
+					onTimeChange={onTimeChange}
+					focusOnSubmit={focusOnSubmit}
+				/>);
+
+				// If value doesn't have legnth 2
+				wrapper.setState({ focusedField: { index: 1, fieldMode: 'minutes' } });
+				const { onSet: onSet1 } = wrapper.find('TimeGroup').at(1).props();
+				onSet1();
+				expect(wrapper.state('shouldHaveFocus')).toEqual(false);
+
+				const { onSet: onSet2 } = wrapper.find('TimeGroup').at(2).props();
+				// If value is minutes,
+				wrapper.setState({ focusedField: { index: 2, fieldMode: 'minutes' } });
+				onSet2('10', '45');
+				expect(wrapper.state('shouldHaveFocus')).toEqual({ index: 3, fieldMode: 'hours' });
+
+				const { onSet: onSet3 } = wrapper.find('TimeGroup').at(3).props();
+				// If value is hours,
+				wrapper.setState({ focusedField: { index: 3, fieldMode: 'hours' } });
+				onSet3('11', '00');
+				expect(wrapper.state('shouldHaveFocus')).toEqual({ index: 3, fieldMode: 'minutes' });
+				// If its last field
+				wrapper.setState({ focusedField: { index: 3, fieldMode: 'minutes' } });
+				onSet3('11', '10');
+				expect(focusOnSubmit).toHaveBeenCalled();
+			});
+		});
+	});
+	describe('componentWillReceiveProps', () => {
+		it('should enable pause if...', () => {
+
+		});
+	});
+});
