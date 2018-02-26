e0fd993617dd9768e85966af9cc555fe09ab9677
henriquefacioli <henrique.facioli@gmail.com>
2018-02-26 10:43:12 -0300

    Using indexedDB instead of weekEntries

--- client/components/edit/MonthlyCalendar.jsx
+++ client/components/edit/MonthlyCalendar.jsx
@@ -7,31 +7,38 @@ import {
 	isDayBlockedInPast,
 	isDayAfterToday
 } from '../../utils';
+import DB from '../../db';
 
 import './MonthlyCalendar.styl';
 
-const _getStyleClassForCalendarDays = (weekEntries) => {
+const _getStyleClassForCalendarDays = async () => {
+	let allEntries = [];
+	try {
+		const db = await DB('entries', 'date');
+		allEntries = await db.getAll();
+	} catch (e) {
+		console.error(e);
+	}
+
 	const dayStyles = [
 		{ 'calendar-checked': [] },
 		{ 'calendar-unchecked': [] },
 		{ 'calendar-locked': [] },
 		{ 'calendar-future-day': [] }
 	];
-	if (weekEntries.timeEntries) {
-		const weekDayNumbers = [1, 2, 3, 4, 5];
-		weekDayNumbers.forEach((day) => {
-			const dayEntries = weekEntries.timeEntries[day];
-			const elementToPush = dayEntries.total ?
+	if (allEntries) {
+		allEntries.forEach((day) => {
+			const elementToPush = day.paidTime ?
 				dayStyles[0]['calendar-checked'] :
 				dayStyles[1]['calendar-unchecked'];
 
-			const dayMoment = moment(dayEntries.date);
+			const dayMoment = moment(day.date);
 
 			elementToPush.push(dayMoment);
 
-			if (isDayBlockedInPast(dayMoment)) {
-				dayStyles[2]['calendar-locked'].push(dayMoment);
-			}
+			// if (isDayBlockedInPast(dayMoment)) {
+			// 	dayStyles[2]['calendar-locked'].push(dayMoment);
+			// }
 			if (isDayAfterToday(dayMoment)) {
 				dayStyles[3]['calendar-future-day'].push(dayMoment);
 			}
@@ -41,26 +48,41 @@ const _getStyleClassForCalendarDays = (weekEntries) => {
 	return dayStyles;
 };
 
-const MonthlyCalendar = ({ controlDate, onDateChange, weekEntries }) => (
-	<DatePicker
-		inline
-		highlightDates={_getStyleClassForCalendarDays(weekEntries)}
-		selected={controlDate}
-		onChange={onDateChange}
-		filterDate={date => date.isSameOrBefore(moment(), 'day')}
-		maxTime={moment()}
-	/>
-);
+class MonthlyCalendar extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			calendarStyle: []
+		};
+	}
+
+	async componentWillMount() {
+		const calendarStyle = await _getStyleClassForCalendarDays();
+		this.setState({ calendarStyle });
+	}
+
+	render() {
+		const { controlDate, onDateChange } = this.props;
+		return (
+			<DatePicker
+				inline
+				highlightDates={this.state.calendarStyle}
+				selected={controlDate}
+				onChange={onDateChange}
+				filterDate={date => date.isSameOrBefore(moment(), 'day')}
+				maxTime={moment()}
+			/>
+		)
+	}
+};
 
 MonthlyCalendar.propTypes = {
 	controlDate: PropTypes.object.isRequired,
-	onDateChange: PropTypes.func,
-	weekEntries: PropTypes.object
+	onDateChange: PropTypes.func
 };
 
 MonthlyCalendar.defaultProps = {
-	onDateChange: () => {},
-	weekEntries: { timeEntries: null }
+	onDateChange: () => {}
 };
 
 export default MonthlyCalendar;
